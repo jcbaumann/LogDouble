4825b8d14c7af14db6bbc96a69bb64b3851fc63f
Jonathan <jonathan@jonathan-baumann.de>
2016-11-04 22:31:20 +0100

    building a frame

---
+++ LogDouble.hpp
@@ -0,0 +1,49 @@
+/**
+ * LogDouble
+ * 
+ * @author Jonathan Baumann
+ */
+
+#include <iostream>
+#include <math.h>
+
+class LogDouble {
+    
+    public:
+    
+        /**
+         * Addition
+         */
+    
+    
+        /**
+         * Substraction
+         */
+    
+    
+        /**
+         * Multiplication
+         */
+    
+    
+        /**
+         * Division
+         */
+    
+    
+        /**
+         * Pow
+         */
+    
+    
+        /**
+         * bit-Shift
+         */
+    
+    
+        /**
+         * Cast to double
+         */
+    
+    
+};
